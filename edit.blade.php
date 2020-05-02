8fc62fc0a0269a452b1ae8017e4fc9af391a5f9c
DebyMonalisaSimanjuntak <64689990+DebyMonalisaSimanjuntak@users.noreply.github.com>
2020-05-02 22:27:10 +0700

    Add files via upload

---
+++ edit.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Laravel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Data Buku</h4>
+            </div>
+            <div class="panel-body">
+                <form action="{{url('update', $data->id)}}" method="post">
+                    <!-- <input type="hidden" name="id" id="id" value="{{$data->id}}"> -->
+                    <div class="form-group">
+                        <label for="kode">Kode Buku</label>
+                        <input type="text" name="kode" id="kode" value="{{$data->kode_buku}}" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="judul">Judul Buku</label>
+                        <input type="text" name="judul" id="judul" value="{{$data->judul}}" class="form-control">
+                    </div>                    
+                    <div class="form-group">
+                        <textarea id="deskripsi" name="deskripsi" class="form-control">
+                            {!!$data->deskripsi!!}
+                        </textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="send" id="send" value="Simpan" class="btn btn-success">{!!csrf_field()!!}                       
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ read.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Laravel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Data Buku</h4>
+            </div>
+            <div class="panel-body">
+                 
+                    <div class="form-group">
+                        <label for="kode">Kode Buku</label>
+                        <input type="text" name="kode" id="kode" value="{{$data->kode_buku}}" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="judul">Judul Buku</label>
+                        <input type="text" name="judul" id="judul" value="{{$data->judul}}" class="form-control" readonly>
+                    </div>                    
+                    <div class="form-group">    
+                        <label for="desk">Deskripsi</label>    
+                        <input type="text" name="desk" id="desk" value="{{$data->deskripsi}}" class="form-control" readonly>
+                    </div>
+                    <form action="{{url('back')}}" method="get">
+                        <div class="form-group">
+                            <input type="submit" value="Back" class="btn btn-success">
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
